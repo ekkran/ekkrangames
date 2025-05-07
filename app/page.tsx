a6f7de486bd25fb5414c9f9ed444c2283f842dba
Elias Giron <josegironf@gmail.com>
2025-05-07 16:21:57 -0600

    working clean

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex flex-col items-center justify-around h-screen bg-black">
-      <h1 className="text-4xl font-bold text-regal-blue-50">EKKRAN GAMES</h1>
-    </main>
-  );
-}
