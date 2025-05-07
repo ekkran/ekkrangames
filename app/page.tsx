d0cdbf7c039c7164ef993e8c1e26df93e783b969
Elias Giron <josegironf@gmail.com>
2025-05-07 14:47:43 -0600

    before migration tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import MainMenu from "@/components/mainMenu";
-
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-around h-screen bg-woodsmoke-950">
-      <h1 className="text-4xl font-bold text-woodsmoke-50">EKKRAN GAMES</h1>
-      <MainMenu></MainMenu>
+    <main className="flex flex-col items-center justify-around h-screen bg-black">
+      <h1 className="text-4xl font-bold text-regal-blue-50">EKKRAN GAMES</h1>
     </main>
   );
 }
